efb2c7715a5dd8c0d293c02868de36dd0452f8e8
William <Billy.campbell17@gmail.com>
2021-01-15 12:00:09 -0500

    updated seed.js

--- db/connections.js
+++ db/connections.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
 let mongoURI = "";
-
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/Fashion";
 }
-mongoose.connect(mongoURI, {
-  useUnifiedTopology: true,
-});
+
+mongoose.connect(mongoURI, { useNewUrlParser: true });
+
 module.exports = mongoose;
---
+++ db/seed.js
@@ -0,0 +1,24 @@
+const WomenFashion = require("../models/WomenFashion");
+const WomenFashionData = require("../db/womenFashion.json");
+
+console.log(WomenFashionData);
+WomenFashion.deleteMany({}).then(() => {
+  WomenFashion.create(WomenFashionData)
+    .then(() => {
+      WomenFashion.find({}).then((res) => console.log(res));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+const menswear = require("../models/menswear");
+const menswearList = require("./menswear.json");
+console.log(menswearList);
+
+menswear.deleteMany({}).then(() => {
+  // Mongoose Model creating data
+  menswear.create(menswearList).then((output) => {
+    console.log(output);
+    process.exit();
+  });
+});
