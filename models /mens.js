740c2671c6c8a04f5ddcb205baf5f3841679684f
William <Billy.campbell17@gmail.com>
2021-01-15 11:55:41 -0500

    updated package.json and models

---
+++ models /mens.js
@@ -0,0 +1,48 @@
+const mongoose = require("../db/connections");
+
+const Schema = mongoose.Schema;
+const Mens = new Schema({
+  logo: String,
+  brand_name: String,
+  CEO: String,
+  type: String,
+  price_point: String,
+  categories: [
+    {
+      category: String,
+      products: [
+        {
+          name: String,
+          image_url: String,
+          details: String,
+          color: String,
+          origin: String,
+        },
+        {
+          name: String,
+          image_url: String,
+          details: String,
+          color: String,
+          origin: String,
+        },
+        {
+          name: String,
+          image_url: String,
+          details: String,
+          color: String,
+          origin: String,
+        },
+        {
+          name: String,
+          image_url: String,
+          details: String,
+          color: String,
+          origin: String,
+        },
+      ],
+    },
+  ],
+  link: String,
+});
+
+module.exports = mongoose.model("Mens", Mens);
